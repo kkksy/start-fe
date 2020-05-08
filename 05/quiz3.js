9ebbfe640c385c9e6ff1347d4701ea0c92d1c517
kkksy <59080486+kkksy@users.noreply.github.com>
2020-05-08 17:00:37 +0900

    07

--- 05/quiz3.js
+++ 05/quiz3.js
@@ -1,57 +1,58 @@
-var $textBtn = document.getElementsByClassName('text-btn')
-var $addRed = document.getElementsByClassName('add-red');
-var $addBox = document.getElementsByClassName('add-btn');
-var $delBox = document.getElementsByClassName('delete-btn');
-var $resetBtn = document.getElementsByClassName('reset-btn');
-var $toggleBtn = document.getElementsByClassName('toggle-btn');
-var $imgBtn = document.getElementsByClassName('image-btn');
-var box = document.querySelectorAll('.box');
+// var $textBtn = document.getElementsByClassName('text-btn')
+// var $addRed = document.getElementsByClassName('add-red');
+// var $addBox = document.getElementsByClassName('add-btn');
+// var $delBox = document.getElementsByClassName('delete-btn');
+// var $resetBtn = document.getElementsByClassName('reset-btn');
+// var $toggleBtn = document.getElementsByClassName('toggle-btn');
+// var $imgBtn = document.getElementsByClassName('image-btn');
+// var box = document.querySelectorAll('.box');
 
 
-var btn_event = {
+// var btn_event = {
 
-    redbox : function() {
+//     redbox : function() {
 
-        box.style.backgroundColor = 'red';
+//         box.style.backgroundColor = 'red';
 
-    },
+//     },
 
-    addbox : function() {
+//     addbox : function() {
 
-        var div = box;
+//         var div = box;
 
         
-    },
+//     },
 
-    delbox : function() {
+//     delbox : function() {
 
-        box.removeChild(obj.parentNode);
+//         box.removeChild(obj.parentNode);
         
-    },
+//     },
 
-    addtext : function() {
+//     addtext : function() {
         
-    },
+//     },
 
-    resetbox : function() {
+//     resetbox : function() {
         
-    },
+//     },
 
-    togglebox : function() {
+//     togglebox : function() {
         
-    },
+//     },
     
-    imgbox : function() {
+//     imgbox : function() {
         
-    }
+//     }
 
 
-}
+// }
 
-$textBtn.addEventListener('click', btn_event.addtext);
-$addRed.addEventListener('click', btn_event.redbox);
-$addBox.addEventListener('click', btn_event.addbox);
-$delBox.addEventListener('click', btn_event.delbox);
-$resetBtn.addEventListener('click', btn_event.resetbox);
-$toggleBtn.addEventListener('click', btn_event.togglebox);
-$imgBtn.addEventListener('click', btn_event.imgbox);
+// $textBtn.addEventListener('click', btn_event.addtext);
+// $addRed.addEventListener('click', btn_event.redbox);
+// $addBox.addEventListener('click', btn_event.addbox);
+// $delBox.addEventListener('click', btn_event.delbox);
+// $resetBtn.addEventListener('click', btn_event.resetbox);
+// $toggleBtn.addEventListener('click', btn_event.togglebox);
+// $imgBtn.addEventListener('click', btn_event.imgbox)
+
---
+++ 07/test.js
@@ -0,0 +1,15 @@
+const str = 'aaa';
+const obj = { 
+    a:1, 
+    b:1, 
+    c:1, 
+    d:1, 
+    f:1, 
+    e:1
+
+};
+
+console.log(obj);
+//.prettierrc 프리티어 설정
+//eslint.js
+//npx eslint --init
