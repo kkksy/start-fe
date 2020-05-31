5b9c41bf114378e77c5445a69f80182d2f816b49
kkksy <59080486+kkksy@users.noreply.github.com>
2020-05-31 18:04:55 +0900

    10

---
+++ 10/src/main.js
@@ -0,0 +1,5 @@
+import sum from './sum'
+
+document.getElementById('debug').innerHTML = sum(1,2);
+
+
---
+++ 10/src/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+  
+  module.exports = {
+    entry: './src/main.js',
+    output: {
+      filename: 'app.js',
+      path: '${__dirname}/dist',
+    },
+    devServer: {
+        contentBase:'./dist',
+    },
+    plugins:{
+        new HtmlWebpackPlugin({
+        template:'./src/index.html',
+    }),
+},
+  };
