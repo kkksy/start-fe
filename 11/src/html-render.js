e348236f491c17fd4f7a921b859ab895d8facdc1
kkksy <59080486+kkksy@users.noreply.github.com>
2020-06-04 17:18:56 +0900

    11

---
+++ 11/src/html-render.js
@@ -0,0 +1,21 @@
+const $result = document.querySelector('#result');
+
+
+export function render(data) {
+
+    const html = todos.map((todo, index) => {
+        return `<li data=index="${index}"> 
+        <button class="delete">×</button>
+        <input type="checkbox" class="toggle-checked" ${
+            todo.isDone ? 'checkded':''
+        }/>
+        <span class="text">${todo.text}</span>
+      </li>`;
+    });
+
+    
+$result.innerHTML = `<ul>${html.join('')}</ul>`;
+
+}
+
+export { render };
---
+++ 11/src/index.js
@@ -0,0 +1,33 @@
+import { render } from './html-render';
+import formInput from './form-input';
+import './todos.css';
+
+const $result = document.querySelector('#result');
+
+const todos = [{
+    text: 'aaa',
+    isDone : false,
+},
+];
+
+$result.addEventListener('click', (event) =>{
+    const { className } = event.target;
+    if(className === 'delete') {
+        
+        const { index } = event.target.parentElment.dataset.index;
+        todos.splice(index, 1);
+        render(todos);
+      
+    }
+
+    else if(className === 'target-checked'){
+
+        const { index } = event.target.parentElment.dataset.index;
+        todos[index].isDone = !todos[index].isDone;
+        render(todos);
+    }
+});
+
+formInput.init(todos);
+render(todos);
+
