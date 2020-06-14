a0e77b37b87a71dac24966721e1b9c0ba3171c13
kkksy <59080486+kkksy@users.noreply.github.com>
2020-06-14 20:38:29 +0900

    11

--- 11/src/data-manager.js
+++ 11/src/data-manager.js
@@ -4,9 +4,16 @@ function saveData(){
     localStorage.setItem('data', JSON.stringify(data));
 }
 
-function loadData() {
+function loadData(callback) {
 
    const dataString = localStorage.getItem('data') || '[]';
    return JSON.parse(dataString)
+
+// fetch('http://localhost:3000/todos').then((response) => {
+//     response.json().then((data) => {
+//         callback(data);
+//     });
+//   });
 }
+
 export {saveData, loadData};
--- 11/src/index.js
+++ 11/src/index.js
@@ -5,26 +5,30 @@ import { loadData } from './data-manager';
 
 const $result = document.querySelector('#result');
 
-const todos = loadData();
+loadData((todos) => {
 
-$result.addEventListener('click', (event) =>{
-    const { className } = event.target;
-    if(className === 'delete') {
-        
-        const { index } = event.target.parentElment.dataset.index;
-        todos.splice(index, 1);
-        render(todos);
-      
-    }
-
-    else if(className === 'target-checked'){
-
-        const { index } = event.target.parentElment.dataset.index;
-        todos[index].isDone = !todos[index].isDone;
-        render(todos);
-    }
+    $result.addEventListener('click', (event) =>{
+        const { className } = event.target;
+        if(className === 'delete') {
+            
+            const { index } = event.target.parentElment.dataset.index;
+            todos.splice(index, 1);
+            render(todos);
+          
+        }
+    
+        else if(className === 'target-checked'){
+    
+            const { index } = event.target.parentElment.dataset.index;
+            todos[index].isDone = !todos[index].isDone;
+            render(todos);
+        }
+    });
+    
+    
+    formInput.init(todos);
+    render(todos);
 });
 
-formInput.init(todos);
-render(todos);
+
 
