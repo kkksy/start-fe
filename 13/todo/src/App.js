dec3aba9f907337790bac5f2249e3fa4099697fd
kkksy <59080486+kkksy@users.noreply.github.com>
2020-06-18 19:17:42 +0900

    13

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 
 function App() {
-  return <div className="App">Hello</div>
+const [todo, setTodo] = useState('');
+const [todos, setTodos] = useState([]);
+
+function handleChange(event) {
+  setTodo(e.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const newTodos = [...todos];
+  newTodos.push(todo);
+  setTodos(newTodos);
+  setTodo('')
+}
+
+console.log(todos);
+
+  return (
+  <div className="App">
+    <div class="container">
+      <header>
+        <Headers/>
+        <h1>Todo List</h1>
+          <form class="new-task" onSubmint = {handleSubmit} >
+      <input name="text" placeholder="할일을 입력하세요" id="input" onChange = {handleChange}/>
+      </form>
+  </header>
+  <div id = "result">
+    <ul>
+    {
+      todos.map(todo => {
+      return(<li>{todo}</li>)
+      })
+    }
+    </ul>
+  </div>
+</div>
+</div>
+  );
 }
 
 export default App;
